4fed1cf739a2def74143e84325fb6b74a8cc8355
Thiago <Thiago Barbosa>
2024-01-14 21:29:09 -0300

    melhorias nos nomes e numeros de participantes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ let amigos = [];
 
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
+    
+    if(amigo.value ==''){
+        alert('informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('nome ja adicionado');
+        return;
+    }
+
+
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
     if(lista.textContent == ''){
@@ -14,6 +26,10 @@ function adicionar(){
 }
 
 function sortear (){
+    if(amigos.length < 4){
+        alert('adicione pelo menos 4 amigos!');
+        return;
+    }
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
